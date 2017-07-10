5a346d89ae8fdd2e68419d67b255df400986e7a1
Veljko Veljkovic <veljkobaka@hotmail.com>
2017-07-10 19:54:21 +0200

    insert date

--- post.php
+++ post.php
@@ -3,21 +3,27 @@
 if ($_POST) {
     $name = json_encode($_POST["name"]);
     $email = json_encode($_POST["email"]);
-    $birthday = $education = isset($_POST["education"]) ? (json_encode($_POST["birthday"])) : "no birthday";
-    $location = $education = isset($_POST["education"]) ? (json_encode($_POST["location"])) : "no location";
-    $education = $education = isset($_POST["education"]) ? (json_encode($_POST["education"])) : "no education";
+    $birthday = isset($_POST["education"]) ? json_encode($_POST["birthday"]) : "no birthday";
+    $location = isset($_POST["education"]) ? json_encode($_POST["location"]) : "no location";
+    $education = isset($_POST["education"]) ? json_encode($_POST["education"]) : "no education";
+    $date = date('Y-m-d-H-i-s');
     
     $query = "SELECT * FROM users WHERE email = $email ";
     $statement = $connection->prepare($query);
     $statement->execute();
     $result = $statement->fetch(PDO::FETCH_ASSOC);
     if($result){
+        $id = $result['id'];
+        $query = "UPDATE users SET name = $name , email = $email , birthday = $birthday, ";
+        $query .= "location = $location, education = $education WHERE id = $id ";
+        $statement = $connection->prepare($query);
+        $statement->execute();
         echo "logged";
         $_SESSION["email"] = $email;
     }else{
         try {
-            $query = "INSERT INTO users (name, email, birthday, location, education) ";
-            $query .= " VALUES ($name, $email, $birthday, $location, $education )";
+            $query = "INSERT INTO users (name, email, birthday, location, education, register_date) ";
+            $query .= " VALUES ($name, $email, $birthday, $location, $education, '$date' )";
             $statement = $connection->prepare($query);
             $statement->execute();
             $_SESSION["email"] = $email;
